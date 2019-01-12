f06fc2d790ff44bed0fce4482dea27aeb735ee1a
Daniel Souza <danielfss98@gmail.com>
2019-01-12 15:00:20 -0200

    Component Header ok

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { Jumbotron } from 'reactstrap';
+import React from 'react';
 
-const Header = () => {
-    return (
-        <Jumbotron>
-            Example page header <small>Subtext for header</small>
+import { Jumbotron, Row, Col } from 'reactstrap';
+import './Header.css';
+
+const Header = (props: any) => {
+  return (
+    <Row>
+      <Col
+        sm={{ size: 10, offset: 1 }}
+        md={{ size: 10, offset: 1 }}
+        lg={{ size: 10, offset: 1 }}
+      >
+        <Jumbotron className="header d-flex justify-content-center">
+          <div className="content d-flex align-items-center">
+            <small>CAMPEONATO DE FILMES</small>
+            <p className="stage">Fase de Seleção</p>
+            <hr />
+            <p className="result">
+              Selecione 8 filmes que voce deseja que entrem na competição e
+              depois pressione o botão Gerar Meu Campenato para prosseguir
+            </p>
+          </div>
         </Jumbotron>
-    )
-}
+      </Col>
+    </Row>
+  );
+};
 
 export default Header;
