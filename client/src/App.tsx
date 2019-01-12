d18f62bfdd99e555a8335130d3f41cdc722ebad0
Daniel Souza <danielfss98@gmail.com>
2019-01-12 19:46:25 -0200

    Add Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Header from './components/header/Header'
-import Main from './screens/Main'
+import Header from './components/header/Header';
+import Main from './screens/Main';
+import Footer from './components/footer/Footer';
 import { Container } from 'reactstrap';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <Container fluid>
         <Header />
         <Main />
+        <Footer />
       </Container>
     );
   }
---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="page-footer font-small">
+        <div className="footer-copyright text-center py-3">
+          <a href="https://github.com/danielfssz">Daniel Souza</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
