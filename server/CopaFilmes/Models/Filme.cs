276826018e0c87b7fc14544512beaa80048bbeeb
Daniel Souza <danielfss98@gmail.com>
2019-01-14 19:20:50 -0200

    Add Model Filme

---
+++ server/CopaFilmes/Models/Filme.cs
@@ -0,0 +1,8 @@
+namespace CopaFilmes.Models {
+    public class Filme {
+        public string Id { get; set; }
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+        public double Nota { get; set; }
+    }
+}
