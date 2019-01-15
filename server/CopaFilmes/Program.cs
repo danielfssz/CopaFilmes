f2556664dba7d685c5bed6e3f5ceb7d270159f3e
Daniel Souza <danielfss98@gmail.com>
2019-01-15 14:51:59 -0200

    Add cors ao projeto

--- server/CopaFilmes/Program.cs
+++ server/CopaFilmes/Program.cs
@@ -2,23 +2,39 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CopaFilmes
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace CopaFilmes {
+    public class Program {
+        public static void Main (string[] args) {
+            // use this to allow command line parameters in the config
+            var configuration = new ConfigurationBuilder ()
+                .AddCommandLine (args)
+                .Build ();
+
+            var hostUrl = configuration["hosturl"];
+            if (string.IsNullOrEmpty (hostUrl)) {
+                string nome = Dns.GetHostName ();
+                IPAddress[] ip = Dns.GetHostAddresses (nome);
+                hostUrl = "http://" + ip[1].ToString () + ":5001";
+            }
+
+            var host = new WebHostBuilder ()
+                .UseKestrel ()
+                .UseUrls (hostUrl) // <!-- this 
+                .UseContentRoot (Directory.GetCurrentDirectory ())
+                .UseIISIntegration ()
+                .UseStartup<Startup> ()
+                .UseConfiguration (configuration)
+                .Build ();
+
+            host.Run ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
